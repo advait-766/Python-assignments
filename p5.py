03d0328cf6e4b161c849d77dc3d1477d6ae617fe
Advait <109798656+advait-766@users.noreply.github.com>
2025-08-24 16:42:12 +0530

    Delete p5.py

--- p5.py
+++
@@ -1,6 +0,0 @@
-numbers = []
-for i in range(5):
-    num = int(input(f"Enter number {i+1}: "))
-    numbers.append(num)
-sum_of_cubes = sum([n**3 for n in numbers])
-print("The sum of cubes of the numbers is:", sum_of_cubes)
