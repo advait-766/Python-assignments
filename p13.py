be67108ac612e836c5e8791eeb2750bc0e9bb437
Advait Khandalkar <advaitkhandalkar766@gmail.com>
2025-08-24 16:46:38 +0530

    Python assignments

--- p13.py
+++
@@ -1,9 +0,0 @@
-def simple_interest(P, R, T):
-    SI = (P * R * T) / 100
-    return SI
-   
-P = float(input("Enter the Principal amount: "))
-R = float(input("Enter the Rate of interest: "))
-T = float(input("Enter the Time (in years): "))
-
-print(f"Simple Interest = Rs {simple_interest(P, R, T):.2f}")
